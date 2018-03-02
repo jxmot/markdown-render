184bf7d977597e8942e140661c0c1be0de3e5cea
jxmot <webdev@stupiddog.net>
2018-03-02 01:26:34 -0600

    initial commit, project CSS and JS files

---
+++ assets/js/totop.js
@@ -0,0 +1,44 @@
+/*
+    As described at (but with major modifications & improvements) - 
+
+        https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+*/
+window.onscroll = function() {onWindowScroll()};
+
+function onWindowScroll() {
+
+    //dump();
+
+    // don't care about safari!
+    //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if(showToTop()) {
+        document.getElementById("gototop").style.display = "block";
+    } else {
+        document.getElementById("gototop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function jumpToTop() {
+    document.body.scrollTop = 0;            // For Safari (who cares? I sure don't! LOL get a REAL computer!)
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function showToTop() {
+    // the point where the the button appears is based on percentage of
+    // the height of the document and NOT the window.
+    if(Math.round(($(document).height() * 0.05)) < document.documentElement.scrollTop) return true;
+    else return false;
+}
+
+// for debugging
+function dump() {
+    console.log($(document).height());
+    console.log(document.documentElement.scrollTop);
+    console.log(Math.round($(document).height() * 0.05));
+    console.log(Math.round(($(document).height() * 0.05)) < document.documentElement.scrollTop);
+    console.log('showToTop() - ' + showToTop());
+    console.log('-------');
+    console.log(document.body.scrollTop);
+    console.log('=======');
+}
