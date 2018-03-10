771f9b76815fd8ff861097812de15e27b42ffe94
jxmot <webdev@stupiddog.net>
2018-03-09 18:05:47 -0600

    added configurable footer & social icons, and "to top" button

--- index.php
+++ index.php
@@ -15,6 +15,13 @@ $pageconfig = new RenderConfig($cfgfile);
 $ElementModifier = new ModifyElements($pageconfig);
 $render = new ParsedownModify($ElementModifier);
 
+global $g_mdpage;
+$mdopt = "./mdrenderpage.json";
+if(file_exists($mdopt))
+{
+    $g_mdpage = json_decode(file_get_contents($mdopt));
+}
+
 // generate a static file?
 if($pageconfig->genstatic === true) ob_start();
 
@@ -37,8 +44,18 @@ if($pageconfig->genstatic === true) ob_start();
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700">
     <!-- page styling -->
     <link rel="stylesheet" href="./assets/css/document.css">
-    <link rel="stylesheet" href="./assets/css/totop.css">
-    <link rel="stylesheet" href="./assets/css/socicon.css">
+
+<?php
+    if(isset($g_mdpage->totop) && ($g_mdpage->totop === true))
+    {
+        echo '<link rel="stylesheet" href="./assets/css/totop.css">';
+    }
+
+    if(isset($g_mdpage->socicon) && ($g_mdpage->socicon === true))
+    {
+        echo '<link rel="stylesheet" href="./assets/css/socicon.css">';
+    }
+?>
     <!-- jquery, etc -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -54,23 +71,43 @@ echo "\n" . $render->text($file) . "\n";
 ?>
             <!-- ^Rendered Document -->
         </div>
-        <button id="gototop" class="gototop" onclick="jumpToTop()" title="Go to top of page">&#9650;<br>top</button> 
+<?php
+    if(isset($g_mdpage->totop) && ($g_mdpage->totop === true))
+    {
+        echo '        <button id="gototop" class="gototop" onclick="jumpToTop()" title="Go to top of page">&#9650;<br>top</button>' . "\n";
+    }
+?>
         <br>
         <br>
     </div>
     <!-- ^Document -->
     <!-- Page Footer -->
+<?php
+    if(isset($g_mdpage->footer) && ($g_mdpage->footer === true))
+    {
+?>
     <footer class="navbar navbar-default navbar-fixed-bottom">
         <div class="text-center">
             <div class="mdfooter col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12">
                 <p>
-                    <a href="https://github.com/jxmot/" target="_blank" class="gi" title="See me on GitHub!"></a>
-                    &nbsp;2017 &copy; James Motyl&nbsp;
-                    <a href="https://www.linkedin.com/in/jim-motyl/" target="_blank" class="in" title="See me on LinkedIn!"></a>
-            </p>
+<?php
+    if(isset($g_mdpage->socicon) && ($g_mdpage->socicon === true))
+    {
+        echo "                    <a href=".$g_mdpage->socitems[0]->url." target=".$g_mdpage->socitems[0]->target." class=".$g_mdpage->socitems[0]->class." title=".$g_mdpage->socitems[0]->title."></a>\n";
+        if(isset($g_mdpage->footertxt))
+        {
+            echo "                    " . $g_mdpage->footertxt . "\n";
+        }
+        echo "                    <a href=".$g_mdpage->socitems[1]->url." target=".$g_mdpage->socitems[1]->target." class=".$g_mdpage->socitems[1]->class." title=".$g_mdpage->socitems[1]->title."></a>\n";
+    }
+?>
+                </p>
             </div>
         </div>
     </footer>
+<?php   
+    }
+?>
     <!-- ^Page Footer -->
 </body>
 <!-- code for the "go to top" button -->
@@ -85,4 +122,4 @@ if($pageconfig->genstatic === true) {
     fwrite($fh,$content);
     fclose($fh);
 }
-?>
+
